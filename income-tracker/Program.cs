aa338d7013e87ceae8e789acf3d929ec2c1b1238
Kociarz <dbednorz@tuta.io>
2024-10-26 17:35:15 +0200

    Improved some functions

--- income-tracker/Program.cs
+++ income-tracker/Program.cs
@@ -1,20 +1,27 @@
 ﻿namespace income_tracker;
 
 // TODO: Make settings working function. 
-// TODO: Make income tracker for year functioning
-// TODO: Make all previus months and year save to a JSON file
+// TODO: Make income tracker for year functioningthe code 
+// Clean up 
 
-// LAST UPDATE: 22 Oct 2024
+// LAST UPDATE: 26 Oct 2024
 
 class Program
 {
     public static int menuChoice;
 
-    public static int expenseAmount;
+    public static int expenseAmount = 1;
+
+    public static int finalExpenseAmount;
     public static string expenseName;
+
+    public static string expenseNameObject;
 
     static void Main(string[] args)
     {
+        List<Expense> expenses = new List<Expense>();  // List to store Expense objects
+
+
         Console.WriteLine("[1] Income tracker for month");
         Console.WriteLine("[2] Income tracker for year");
         Console.WriteLine("[3] Settings");
@@ -37,16 +44,37 @@ class Program
         switch(menuChoice){
             case 1:
                 System.Console.WriteLine("Enter all expenses this month");
-                System.Console.Write("Expense name (workers salary, tools, software ect.): ");
-                expenseName = Console.ReadLine();
+                
 
-                System.Console.Write("Expense ammount (in USD): ");
-                expenseAmount = int.Parse(Console.ReadLine());
+                while(expenseAmount > 0){
+                    System.Console.Write("Expense name (workers salary, tools, software ect.): ");
+                    expenseName = Console.ReadLine();
 
-                Expense newExpense = new Expense{
-                    _expenseName = expenseName,
-                    _expenseAmmount = expenseAmount
-                };
+                    System.Console.Write("Expense ammount (in USD) (type 0 to exit): ");
+                    expenseAmount = int.Parse(Console.ReadLine());
+
+                    if(expenseAmount == 0)
+                        break;
+
+                    Expense expense = new Expense{
+                        _expenseName = expenseName,
+                        _expenseAmmount = expenseAmount
+
+                        
+                    };
+                    expenses.Add(expense);
+
+
+                }
+
+                Console.WriteLine("\nList of all expenses:");
+                foreach (var expense in expenses)
+                {
+                    Console.WriteLine($"Expense Name: {expense._expenseName}");
+                }
+                
+
+                
 
                 break;
             
@@ -69,4 +97,8 @@ class Program
 public class Expense(){
     public string _expenseName {get; set;}
     public int _expenseAmmount {get; set;}
+
+    public static void objectCreatedCheck(){
+        System.Console.WriteLine("Object created");
+    }
 }
